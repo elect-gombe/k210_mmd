16265d162b05f1d132e999ec3385991a3e6ab7c6
elect-gombe <13552050+elect-gombe@users.noreply.github.com>
2019-03-15 17:29:32 +0900

    Add fileio.h

---
+++ fileio.h
@@ -0,0 +1,33 @@
+#ifndef _FILE_IO_H
+#define _FILE_IO_H
+
+
+#if PC
+typedef void* fil;
+#endif
+
+#if defined(__cplusplus)
+extern "C"{
+#endif
+#if 0
+}
+#endif
+//file open
+//return 0 if success
+int filopen(const char *pathname,fil *f);
+
+//file read
+int filread(fil fp,void *buff,size_t byte);
+
+//fail
+static inline
+void fail(){
+  //todo
+  while(1);
+}
+
+#if defined(__cplusplus)
+};
+#endif
+
+#endif//_FILE_IO_H
